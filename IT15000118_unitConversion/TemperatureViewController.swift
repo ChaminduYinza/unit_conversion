637793bcf3efd346a2ab53d7cc974be2484bf770
Chamindu Thiranjaya <chaminduthiranjaya@chamindus-mac.local>
2018-08-09 21:13:37 +0530

    weight tab ui

--- IT15000118_unitConversion/TemperatureViewController.swift
+++ IT15000118_unitConversion/TemperatureViewController.swift
@@ -1,5 +1,5 @@
 //
-//  TemperaturViewController.swift
+//  TemperatureViewController.swift
 //  IT15000118_unitConversion
 //
 //  Created by Chamindu Thiranjaya on 8/9/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TemperaturViewController: UIViewController {
+class TemperatureViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
