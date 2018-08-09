2882fa83213938e5860d45834f787ebf2815aa06
Uditha Kasun <chaminduthiranjaya@chamindus-mac.local>
2018-08-09 20:18:07 +0530

    added tab view controllers for weight, temperature, distance and speed

---
+++ IT15000118_unitConversion/TemperaturViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TemperaturViewController.swift
+//  IT15000118_unitConversion
+//
+//  Created by Chamindu Thiranjaya on 8/9/18.
+//  Copyright © 2018 Chamindu Thiranjaya. All rights reserved.
+//
+
+import UIKit
+
+class TemperaturViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ IT15000118_unitConversion/WeightViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WeightViewController.swift
+//  IT15000118_unitConversion
+//
+//  Created by Chamindu Thiranjaya on 8/9/18.
+//  Copyright © 2018 Chamindu Thiranjaya. All rights reserved.
+//
+
+import UIKit
+
+class WeightViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
